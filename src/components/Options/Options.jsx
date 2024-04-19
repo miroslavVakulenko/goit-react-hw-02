f167f6581a4c794446f5511135f9bbad8cad25d8
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-04-19 14:32:47 +0300

    jsx

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,10 @@
+export default function Options() {
+  return (
+    <>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <button>Reset</button>
+    </>
+  )
+}
