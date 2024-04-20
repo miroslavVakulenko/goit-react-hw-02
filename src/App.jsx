1fcf09feec19b1829203da1e70d4c897bcb7711e
vanger228 <112012329+vanger228@users.noreply.github.com>
2024-04-20 13:40:06 +0300

    try use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,13 +6,25 @@ import Description from './components/Description/Description'
 import Options from './components/Options/Options'
 import Feedback from './components/Feedback/Feedback'
 function App() {
-  
+  const [obj, setObj] = useState({
+	good: 0,
+	neutral: 0,
+	bad: 0
+  })
+  const handleChangeFeedback = () => {
+    return setObj({
+      ...obj,
+      good: { goodValue },
+      neutral: { neutralValue },
+      bad: {badvalue}
+    })
+  }
 
   return (
     <>
       <Description />
       <Options />
-      <Feedback/>
+      <Feedback props={handleChangeFeedback} />
     </>
   )
 }
